3803e1a0b908175b544730b81fc02a97bdb1c202
Rik2m6 <henrique.sousas100@gmail.com>
2026-07-02 02:09:14 -0300

    update: 01/07/26

---
+++ software/dashboard/windows/frame/Workspace_config.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+class PainelConfig(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+
+    def create_widgets(self):
+        self.closeWS()
+        labelteste = tk.Label(self.master, text="configPainel")
+        labelteste.pack()
+
+    def show(self):
+        self.create_widgets()
+        
+    def closeWS(self):
+        for widget in self.master.winfo_children():
+            widget.destroy()
